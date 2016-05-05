4bb2811c3fe600f0b414cd711102ddf0735ae1e1
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-05-05 09:08:30 +0300

    Initial Commit

---
+++ STMBTDataTransfer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STMBTDataTransfer
+//
+//  Created by Maxim Grigoriev on 05/05/16.
+//  Copyright © 2016 Sistemium UAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
